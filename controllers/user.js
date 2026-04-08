24aecac0aaac432ca81746f90b7c9762cbfb128b
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2026-04-08 09:49:30 +0530

    chore: remove similar feature route Due to change from username only access to password based access to the api remove previous similar functionality routes

--- controllers/user.js
+++ controllers/user.js
@@ -22,21 +22,3 @@ export const getUser = async (req, res, next) => {
     next(err);
   }
 };
-
-export const postUser = async (req, res, next) => {
-  const { username } = req.body;
-
-  try {
-    const userData = await db.createUser(username);
-    return res.json({ userData });
-  } catch (err) {
-    if (err instanceof PrismaClientKnownRequestError) {
-      return res.status(400).json({
-        message: "username already exists",
-        code: err.code,
-      });
-    }
-
-    next(err);
-  }
-};
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,9 @@
 import { Router } from "express";
 
-import { getUser, postUser } from "./../controllers/user.js";
+import { getUser } from "./../controllers/user.js";
 
 const userRouter = Router();
 
 userRouter.get("/", getUser);
-
-userRouter.post("/", postUser);
 
 export default userRouter;
