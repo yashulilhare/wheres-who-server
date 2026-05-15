d3d1bb810c266226f452ba250f1e6fb2df59474b
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2026-05-15 09:41:32 +0530

    add fallback port if not given in .env variables

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import app from "./app.js";
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, (err) => {
   if (err) throw new Error(err);
